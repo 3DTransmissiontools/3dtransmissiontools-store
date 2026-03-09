0f12582cb14444a9f085e147787e658b8f12d29a
3DTransmissiontools <pctransmissions@yahoo.com>
2026-03-09 14:10:03 -0700

    Update create-checkout-session.js

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -11,21 +11,30 @@ export default async function handler(req, res) {
     const session = await stripe.checkout.sessions.create({
       mode: "payment",
       payment_method_types: ["card"],
+
+      automatic_tax: {
+        enabled: true
+      },
+
       line_items: [
         {
           price_data: {
             currency: "usd",
+            tax_behavior: "exclusive",
             product_data: {
-              name: "6L80 / 6L90 Solenoid Tool"
+              name: "6L80 / 6L90 Solenoid Tool",
+              tax_code: "txcd_99999999"
             },
             unit_amount: 4995
           },
           quantity: 1
         }
       ],
+
       shipping_address_collection: {
         allowed_countries: ["US"]
       },
+
       success_url: "https://3dtransmissiontools.com/success.html",
       cancel_url: "https://3dtransmissiontools.com/cancel.html"
     });
