f68de7820a4ed028666789dca18f88339bf62beb
3DTransmissiontools <pctransmissions@yahoo.com>
2026-03-10 14:54:59 -0700

    Update orders.js

--- api/orders.js
+++ api/orders.js
@@ -1,1 +1,48 @@
+import fs from "fs";
+import path from "path";
 
+const ordersFile = path.join(process.cwd(), "api", "orders-data.json");
+
+export default function handler(req, res) {
+
+if (!fs.existsSync(ordersFile)) {
+
+return res.json([]);
+
+}
+
+let orders = JSON.parse(fs.readFileSync(ordersFile));
+
+if (req.method === "GET") {
+
+return res.json(orders);
+
+}
+
+if (req.method === "POST") {
+
+const { id, shipped, tracking } = req.body;
+
+orders = orders.map(order => {
+
+if (order.id === id) {
+
+return {
+...order,
+shipped,
+tracking
+};
+
+}
+
+return order;
+
+});
+
+fs.writeFileSync(ordersFile, JSON.stringify(orders, null, 2));
+
+return res.json({ success: true });
+
+}
+
+}
