2dd285a66a10c9a0afc8cdce598df9e726008b62
3DTransmissiontools <pctransmissions@yahoo.com>
2026-08-27 15:25:09 -0700

    Add Pirate Ship order export (#12)
    Add a Store Admin CSV export for paid, unshipped orders that can be uploaded to Pirate Ship.

--- api/orders.js
+++ api/orders.js
@@ -129,6 +129,9 @@ async function formatOrder(stripe, session) {
     tracking:
       session.metadata?.tracking || "",
 
+    package_weight_oz:
+      Number(session.metadata?.package_weight_oz || 0),
+
     items: lineItems.data.map(item => ({
       name: item.description || "Item",
 
@@ -380,3 +383,4 @@ export default async function handler(req, res) {
     });
   }
 }
+
